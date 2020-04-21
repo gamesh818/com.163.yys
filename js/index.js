fe91b9c39ecccc2ddf62303c7a58daf1cbb23c68
gamesh818 <544937399@qq.com>
2020-04-21 18:33:34 +0800

    提交4/21项目

--- js/index.js
+++ js/index.js
@@ -30,13 +30,85 @@ $(function(){
         flag--
        }
     })
-    
+    // 鼠标移动到伪轮播图盒子上的时候 盒子往上跳动  js不适用 已经转用css hover解决
+    // $('.main-center-lun > div').on('mouseenter',function(){
+    //     var top = $(this).position().top;
+    //     top=top-10
+    //     $(this).stop().animate({
+    //         top:top
+    //     },300)
+    // })
+    // $('.main-center-lun > div').on('mouseleave',function(){
+    //     var top = $(this).position().top;
+    //     top=top+10
+    //     $(this).stop().animate({
+    //         top:top
+    //     },300)
+    // })
 
 
-
-
-
-
+    // 主页伪轮播图
+    // 点击任意一个 小盒子显示遮罩层和大盒子
+    $('.main-center-lun > div').on('click',function(){
+        let index = $(this).index()
+        // 隐藏二维码
+        $('.left-erwei').hide()
+        // 显示遮罩层
+        $('.lun-black').show()
+        // 显示大盒子
+        $('.lun-bigbox').show()
+        // 显示当前点击对应的盒子 并隐藏其他同辈盒子
+        $('.lun-conter').eq(index).css("opacity", 1).siblings().css("opacity", 0)
+        // 点击关闭按钮 隐藏盒子和遮罩层 显示二维码
+        $('.lun-stop').on('click',function(){
+            // 显示遮罩层
+        $('.lun-black').hide()
+        // 显示大盒子
+        $('.lun-bigbox').hide()
+        // 显示二维码
+        $('.left-erwei').show()
+        })
+        // 点击左右按钮 更换对应的盒子
+        $('.lun-left').on('click',function(){
+            if(index !=0){
+                index--
+                $('.lun-conter').eq(index).css("opacity", 1).siblings().css("opacity", 0)
+            }
+            // 判断图片是否是第一张或者是最后一张 如果是则给响应的按钮添加样式 和禁用按钮
+            if(index == 0){
+                $('.lun-left').addClass("lun-butstop")
+            }else if(index == 5){
+                $('.lun-right').addClass("lun-butstop")
+            }else{
+                $('.lun-left').removeClass("lun-butstop")
+                $('.lun-right').removeClass("lun-butstop")
+            }
+        })
+        $('.lun-right').on('click',function(){
+            if(index !=5){
+                index++
+                $('.lun-conter').eq(index).css("opacity", 1).siblings().css("opacity", 0)
+            }
+            // 判断图片是否是第一张或者是最后一张 如果是则给响应的按钮添加样式 和禁用按钮
+            if(index == 0){
+                $('.lun-left').addClass("lun-butstop")
+            }else if(index == 5){
+                $('.lun-right').addClass("lun-butstop")
+            }else{
+                $('.lun-left').removeClass("lun-butstop")
+                $('.lun-right').removeClass("lun-butstop")
+            }
+        })
+        // 判断图片是否是第一张或者是最后一张 如果是则给响应的按钮添加样式 和禁用按钮
+        if(index == 0){
+            $('.lun-left').addClass("lun-butstop")
+        }else if(index == 5){
+            $('.lun-right').addClass("lun-butstop")
+        }else{
+            $('.lun-left').removeClass("lun-butstop")
+            $('.lun-right').removeClass("lun-butstop")
+        }
+    })
 
 
 
